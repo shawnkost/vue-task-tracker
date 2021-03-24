e601fe3462beb6974c1c7d5678693b0006171ca0
Shawn Kost <shawnmkost@gmail.com>
2021-03-24 12:24:44 -0700

    Created a footer and started working on routing functionality

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,14 @@
       @delete-task="deleteTask"
       :tasks="tasks"
     />
+    <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
@@ -27,6 +30,7 @@ export default {
     Header,
     Tasks,
     AddTask,
+    Footer
   },
   data() {
     return {
